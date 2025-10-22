0641d7ed65a736a0fd37bf1406afb6077481c274
hoadx <hoadx@arrowhitech.com>
2025-10-22 16:50:27 +0700

    ADD

--- en_base_import/__manifest__.py
+++ en_base_import/__manifest__.py
@@ -6,8 +6,8 @@
 ===========================
     Base Action Export/Import Detail
 """,
-    "category": "Extra Tools",
-    "author": "Cloud Open Technologies",
+    'author': 'Cloud Open Technologies/K8 Team',
+    'category': 'Services, Import, Import One2many',
     "version": "1.0.0",
     "depends": ['base','base_import'],
     'data': [
